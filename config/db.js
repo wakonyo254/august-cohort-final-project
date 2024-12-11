b1e433f0250bb96ef98f91f2e31da2bdd32c7807
wakonyo254 <wakonyomacharia003@gmail.com>
2024-12-11 16:37:39 +0300

    db table addition on progress

--- config/db.js
+++ config/db.js
@@ -12,4 +12,33 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME,
 });
 
+const createChildrenTable = `
+CREATE TABLE IF NOT EXISTS test (
+child_id INT AUTO_INCREMENT PRIMARY KEY,
+first_name(255) NOT NULL,
+last_name(255) NOT NULL,
+email VARCHAR(200) NOT NULL,
+date_of_birth DATE,
+password_hash VARCHAR(255),
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+`;
+
+pool.query(createChildrenTable, (err, result) =>{
+    if(err) {
+        console.err('Error creating test table', err.message);
+        return;
+    }
+    console.log('Test table successfully created!!!');
+
+    //close pool
+    pool.end((err) =>{
+        if(err) {
+            console.err('Error closing pool', err.message);
+            return;
+        }
+        console.log('Pool closed successfully!!');
+    });
+});
+
 module.exports = pool.promise();
