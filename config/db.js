a23fd6bb742a089a9bf6cb28e549723a022a9c59
wakonyo254 <wakonyomacharia003@gmail.com>
2024-12-12 15:06:03 +0300

    successfully added the database tables

--- config/db.js
+++ config/db.js
@@ -12,33 +12,92 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME,
 });
 
-const createChildrenTable = `
-CREATE TABLE IF NOT EXISTS test (
+const createChildrenTable  = `
+CREATE TABLE IF NOT EXISTS children (
 child_id INT AUTO_INCREMENT PRIMARY KEY,
-first_name(255) NOT NULL,
-last_name(255) NOT NULL,
-email VARCHAR(200) NOT NULL,
+first_name VARCHAR(255) NOT NULL,
+last_name VARCHAR(255) NOT NULL,
+email VARCHAR(200) NOT NULL UNIQUE,
 date_of_birth DATE,
 password_hash VARCHAR(255),
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 `;
+const createParentTable  = `
+CREATE TABLE IF NOT EXISTS parents (
+parent_id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL UNIQUE,
+password_hash VARCHAR(255),
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+`;
 
-pool.query(createChildrenTable, (err, result) =>{
-    if(err) {
-        console.err('Error creating test table', err.message);
-        return;
-    }
-    console.log('Test table successfully created!!!');
+const createChallengesTable  = `
+CREATE TABLE IF NOT EXISTS challenges (
+challenge_id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(255) NOT NULL,
+description TEXT NOT NULL,
+points INT NOT NULL,
+status ENUM('active', 'archived') DEFAULT 'active',
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+`;
+const createAdminTable  = `
+CREATE TABLE IF NOT EXISTS admin (
+admin_id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+email VARCHAR(200) NOT NULL,
+password_hash VARCHAR(255) NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+`;
 
-    //close pool
-    pool.end((err) =>{
+const createAdminlogTable  = `
+CREATE TABLE IF NOT EXISTS adminlogs (
+log_id INT AUTO_INCREMENT PRIMARY KEY,
+admin_id INT NOT NULL,
+action TEXT NOT NULL,
+timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY(admin_id) REFERENCES admin(admin_id)
+);
+`;
+
+const createReawardTable  = `
+CREATE TABLE IF NOT EXISTS rewards (
+    reward_id INT AUTO_INCREMENT PRIMARY KEY,
+    child_id INT NOT NULL,
+    total_points INT NOT NULL,
+    reward_description TEXT,
+    redeemed_at TIMESTAMP,
+    FOREIGN KEY (child_id) REFERENCES Children(child_id)
+);
+`;
+
+const createProgressTable  = `
+CREATE TABLE IF NOT EXISTS progress (
+    progress_id INT AUTO_INCREMENT PRIMARY KEY,
+    child_id INT NOT NULL,
+    challenge_id INT NOT NULL,
+    date_completed DATE NOT NULL,
+    FOREIGN KEY (child_id) REFERENCES Children(child_id),
+    FOREIGN KEY (challenge_id) REFERENCES Challenges(challenge_id)
+);
+
+`;
+
+// 
+const queries = [createChildrenTable, createParentTable, createChallengesTable,
+     createAdminTable, createAdminlogTable, createReawardTable, createProgressTable];
+
+queries.forEach((query) => {
+    pool.query(query, (err, results) =>{
         if(err) {
-            console.err('Error closing pool', err.message);
-            return;
+            console.error(`Error executing query: ${query}`, err.message);
+        } else {
+            console.log('database tables successfully created!');
         }
-        console.log('Pool closed successfully!!');
     });
-});
+});     
 
 module.exports = pool.promise();
